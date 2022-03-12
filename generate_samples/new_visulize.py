74af3c2df987444ef3ab74b1f8161d81ebe92a94
kzhang19 <65601837+zhangkai0425@users.noreply.github.com>
2022-03-12 21:52:36 +0800

    Add files via upload

---
+++ generate_samples/new_visulize.py
@@ -0,0 +1,102 @@
+import os
+import re
+import sys
+import math
+import cv2 as cv
+sys.path.append("../../")
+import torch
+from utils import *
+import numpy as np
+import torch.nn as nn
+from PIL import Image
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from getedges import getedges
+from generate_samples import generate_samples
+from pytorch3d.structures import Meshes, Pointclouds, join_meshes_as_scene, join_meshes_as_batch
+from pytorch3d.renderer.mesh.rasterizer import Fragments
+from pytorch3d.renderer.mesh.textures import TexturesUV
+
+def generate_3D_edges(file_path='/hdd1/zhangkai/256X40',mesh_resol=3,atlas_resol=1024,num_atlas = 40,Edge_Id = None,ALL_Edge = None):
+
+    # file_path = '/hdd1/zhangkai/256X40'
+    # mesh_resol = 3 # 读取3.obj文件
+    # atlas_resol = 128
+    # num_atlas = 40
+
+    # '''
+    # 上面是全局的参数
+    # '''
+    selected_atlas_indices = [i for i in range(num_atlas)]
+    # Set the device
+    if torch.cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
+        print("WARNING: CPU only, this will be slow!")
+
+    verts, faces, aux = load_obj(file_path+'/%s.obj'%mesh_resol)
+    verts_uvs = aux.verts_uvs
+    verts_uvs = (verts_uvs * (atlas_resol - 1.)).type(torch.cuda.LongTensor).cpu().numpy()
+    print('debug here:',verts_uvs.shape)
+    verts_atlas, faces_atlas, verts_uvs_atlas, faces_uvs_atlas, z_embedding_length = coarse_atlas_import(
+            file_path = file_path,
+            mesh_resol = mesh_resol)
+    atlas_mesh_list = mesh_from_list(
+        verts_atlas, faces_atlas, verts_uvs_atlas, faces_uvs_atlas, z_embedding_length,selected_atlas_indices = selected_atlas_indices
+    )
+    Inner_Edge_3D = []
+    Out_Edge_3D = []
+    for i in range(len(Edge_Id)):
+        # 内边界
+        atlas_mesh = atlas_mesh_list[Edge_Id[i][0]]
+        outlier_mask, pixel_facet_IDM, barycentric = initialize_atlas_uv_map(resol=atlas_resol,mesh=atlas_mesh)
+        image_interpolate = generate_init_GM(barycentric=barycentric,outlier_mask=outlier_mask,
+            pixel_facet_IDM=pixel_facet_IDM,resol=atlas_resol,mesh=atlas_mesh,atlas_id=Edge_Id[i][0])
+        XY = (ALL_Edge[0][i] * (atlas_resol-1)).astype(int)
+        Inner_Edge_3D += [image_interpolate[XY[:,1],XY[:,0],:]]
+
+        # 外边界
+        atlas_mesh = atlas_mesh_list[Edge_Id[i][1]]
+        outlier_mask, pixel_facet_IDM, barycentric = initialize_atlas_uv_map(resol=atlas_resol,mesh=atlas_mesh)
+        image_interpolate = generate_init_GM(barycentric=barycentric,outlier_mask=outlier_mask,
+            pixel_facet_IDM=pixel_facet_IDM,resol=atlas_resol,mesh=atlas_mesh,atlas_id=Edge_Id[i][0])
+        XY = (ALL_Edge[1][i] * (atlas_resol-1)).astype(int)
+        Out_Edge_3D += [image_interpolate[XY[:,1],XY[:,0],:]]
+        print("atlas_id:",Edge_Id[i],"done!","当前进度:%s/%s"%(i,len(Edge_Id)))
+    ALL_Edge_3D = []
+    ALL_Edge_3D.append(Inner_Edge_3D)
+    ALL_Edge_3D.append(Out_Edge_3D)
+    return ALL_Edge_3D
+
+if __name__ == '__main__':
+    Edge_Id = torch.load("Edge_Id_1024.pt")
+    ALL_Edge = torch.load("ALL_Edge_1024.pt")
+    ALL_Edge_3D = generate_3D_edges(file_path='/hdd1/zhangkai/256X40',mesh_resol=3,atlas_resol=1024,num_atlas = 40,Edge_Id = Edge_Id,ALL_Edge = ALL_Edge)
+
+    XYZ = np.array([[0,0,0]])
+    RGB = np.array([[0,0,0]])
+
+    for i in range(len(Edge_Id)):
+        # 内边界上色
+        XYZ = np.concatenate((XYZ,ALL_Edge_3D[0][i]),axis = 0)
+        row = len(ALL_Edge_3D[0][i])
+        rgb = np.zeros((row,3))
+        rgb[:,0] = (10 + 5*i + 10 * Edge_Id[i][0]/200)%255
+        rgb[:,1] = (255 - 10 * Edge_Id[i][0])%230
+        rgb[:,2] = 10 + (33 * i)%240
+        RGB = np.concatenate((RGB,rgb),axis = 0)
+        # 外边界上色
+        XYZ = np.concatenate((XYZ,ALL_Edge_3D[1][i]),axis = 0)
+        row = len(ALL_Edge_3D[1][i])
+        rgb = np.zeros((row,3))
+        rgb[:,0] = (10 + 5*i + 10 * Edge_Id[i][1]/200)%255
+        rgb[:,1] = (255 - 10 * Edge_Id[i][1])%230
+        rgb[:,2] = 10 + (33 * i)%240
+        RGB = np.concatenate((RGB,rgb),axis = 0)   
+    
+    XYZ = XYZ[1:]
+    RGB = RGB[1:]
+    print(XYZ.shape,RGB.shape)
+
+    save2ply(ply_filePath="visulize/3D/edges_1024.ply",xyz_np=XYZ,rgb_np=RGB)
